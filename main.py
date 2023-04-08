3fbb243cbaab39826a40a76b8a2475163834f172
AxelCrypto <axelgirougarcia@gmail.com>
2023-04-08 11:57:15 +0200

    deployment

--- main.py
+++ main.py
@@ -39,9 +39,7 @@ st.sidebar.image('images/logo.png', width = 250)
 
 
 
-st.title(' :blue[Metrum]')
-st.header('Exercice de recrutement (BI), `Bonus`')
-
+st.title(' :blue[Metrum], Exercice de recrutement (BI), `Bonus`')
 
 
 
